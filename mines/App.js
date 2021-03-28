a69054021e9ccf7f8f0919d7bed30983d46d8b24
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2021-03-28 14:40:49 -0300

    20200328

---
+++ mines/App.js
@@ -0,0 +1,63 @@
+import React, { Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import params from './src/params'
+import Field from './src/components/Field'
+import Flag from './src/components/Flag'
+import MineField from './src/components/MineField'
+import {
+  createMinedBoard
+} from './src/functions'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
+  render(){
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text >Welcome</Text>
+        <Text>Tamanho da grade
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <View style={styles.board} >
+          <MineField board={this.state.board} />
+        </View>
+      </SafeAreaView>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
+});
